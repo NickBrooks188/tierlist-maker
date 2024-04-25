8e629f2e16254403d08ea96bc08ef9016e3e4af7
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-04-25 16:41:15 -0700

    adding test routes

--- backend/tierlist/views.py
+++ backend/tierlist/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import Http404, HttpResponseNotAllowed, HttpResponse
+from .models import Card, User, ListPublished, ListTemplate
+
 
 # Create your views here.
+def test(request):
+    query = Card.objects.all()
+    data = serializers.serialize("json", query)
+    return HttpResponse(query)
