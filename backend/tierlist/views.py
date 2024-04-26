65ade275c824e02e1f1a6ba0c7b12cde35f1eb7b
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-04-25 18:37:38 -0700

    Tinkering with foreign key queries

--- backend/tierlist/views.py
+++ backend/tierlist/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import Http404, HttpResponseNotAllowed, HttpResponse
 from .models import Card, User, ListPublished, ListTemplate
-
+import json
 
 # Create your views here.
 def test(request):
     list = ListTemplate.objects.filter(id=1)
     cards = list[0].card_set.all()
-    data1 = serializers.serialize("json", list)
-    data2 = serializers.serialize("json", cards)
-    data3 = {}
-    data3['list'] = list
-    data3['cards'] = cards
-    print(data3)
-    print(serializers.serialize("json", data3))
-    return HttpResponse(data3)
+    list_with_cards = {}
+    list_data = serializers.serialize("json", list)
+    cards_data = serializers.serialize("json", cards)
+    list_dict = json.loads(list_data)
+    cards_dict = json.loads(cards_data)
+    list_with_cards['list'] = list_dict
+    list_with_cards['cards'] = cards_dict
+    return HttpResponse(json.dumps(list_with_cards))
