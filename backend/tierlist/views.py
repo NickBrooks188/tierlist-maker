51cc99a28ecd27af34b39ce5cf70d16d87f2b282
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-04-29 17:16:51 -0700

    Update views.py

--- backend/tierlist/views.py
+++ backend/tierlist/views.py
@@ -23,59 +23,6 @@ class ListPublishedSerializer(serializers.ModelSerializer):
         model = ListPublished
         fields = ["id", "name", "description", "public", "s_tier", "a_tier", "b_tier", "c_tier", "d_tier", "f_tier", "owner", "template"]
 
-# Create your views here.
-def get_list_template_by_id(request, template_id):
-    list_template = ListTemplate.objects.filter(id=template_id)
-    try:
-        cards = list_template[0].card_set.all()
-    except:
-        # placeholder for list not found, might be able to speed up with ge_object_or_404
-        return HttpResponse(status=404)
-    list_with_cards = {}
-    list_data = serializers.serialize("json", list_template)
-    cards_data = serializers.serialize("json", cards)
-    list_dict = json.loads(list_data)
-    cards_dict = json.loads(cards_data)
-    list_with_cards['list'] = list_dict[0]
-    list_with_cards['list']['cards'] = cards_dict
-    return HttpResponse(json.dumps(list_with_cards))
-
-
-def get_all_list_templates(request):
-    # can potential use ListTemplate.object.values() to clean this up
-    lists = ListTemplate.objects.all()
-    lists_return = serializers.serialize("json", lists)
-    lists_return = json.loads(lists_return)
-    for i in range(len(lists)):
-        cards = lists[i].card_set.all()
-        cards_data = serializers.serialize("json", cards)
-        cards_dict = json.loads(cards_data)
-        lists_return[i]['cards'] = cards_dict
-    return HttpResponse(json.dumps(lists_return))
-
-def get_all_published_lists(request):
-    lists = ListPublished.objects.all()
-    lists_return = serializers.serialize("json", lists)
-    return HttpResponse(lists_return)
-
-def get_published_list_by_id(request, list_id):
-    published_list = ListPublished.objects.filter(id=list_id)
-    list_data = serializers.serialize("json", published_list)
-    list_dict = json.loads(list_data)
-    list_return = list_dict[0]
-    # try:
-    #     cards = list[0].card_set.all()
-    # except:
-    #     return HttpResponse(status=404)
-    # list_with_cards = {}
-    # list_data = serializers.serialize("json", list)
-    # cards_data = serializers.serialize("json", cards)
-    # list_dict = json.loads(list_data)
-    # cards_dict = json.loads(cards_data)
-    # list_with_cards['list'] = list_dict[0]
-    # list_with_cards['list']['cards'] = cards_dict
-    return HttpResponse(json.dumps(list_return))
-
 # signup
 def sign_up(request):
     print(request)
@@ -161,10 +108,29 @@ class TemplatesOne(APIView):
             status=status.HTTP_200_OK
         ) 
 
-
-
+class PublishedAll(APIView):
 # GET /published
+    def get(self, request, *args, **kwargs):
+        published = ListPublished.objects.filter(user = request.user.id)
+        serializer = ListTemplateSerializer(published, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
 # POST /published
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.data.get('name'), 
+            'description': request.data.get('description'),
+            'public': request.data.get('public'), 
+            'owner': request.user.id,
+            'template': request.data.get('template_id')
+        }
+        serializer = ListPublishedSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # DELETE /published/<template_ID>
 # PUT /published/<template_ID>
