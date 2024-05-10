6284907739874f00a9717cd00fcc9d996820e118
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-05-10 15:26:47 -0700

    Published tile styling + dynamic preview

--- tierlist/app/components/PublishedTile/publshedtile.tsx
+++ tierlist/app/components/PublishedTile/publshedtile.tsx
@@ -4,19 +4,40 @@ import styles from "./PublishedTile.module.css";
 interface PublishedTileProps {
     name: string,
     description: string,
-    s_tier: object,
-    a_tier: object,
-    b_tier: object,
-    c_tier: object,
-    d_tier: object,
-    f_tier: object,
+    s_tier: [],
+    a_tier: [],
+    b_tier: [],
+    c_tier: [],
+    d_tier: [],
+    f_tier: [],
 }
 
 export default function PublishedTile({ name, description, s_tier, a_tier, b_tier, c_tier, d_tier, f_tier }: PublishedTileProps) {
+    const maxLength: number = Math.max(s_tier.length, a_tier.length, b_tier.length, c_tier.length, d_tier.length, f_tier.length)
+    const sLength = (s_tier.length / maxLength) * 136 + 20
+    const aLength = (a_tier.length / maxLength) * 136 + 20
+    const bLength = (b_tier.length / maxLength) * 136 + 20
+    const cLength = (c_tier.length / maxLength) * 136 + 20
+    const dLength = (d_tier.length / maxLength) * 136 + 20
+    const fLength = (f_tier.length / maxLength) * 136 + 20
+
+    console.log(sLength, aLength, bLength, cLength, dLength, fLength)
+
     return (
         <div className={styles.published_tile} >
-            {name}
-            <div className={styles.publsihed_tile_description}>{description}</div>
+            <div className={styles.published_tile_preview}>
+                <div className={styles.s_preview} style={{ "width": sLength }} />
+                <div className={styles.a_preview} style={{ "width": aLength }} />
+                <div className={styles.b_preview} style={{ "width": bLength }} />
+                <div className={styles.c_preview} style={{ "width": cLength }} />
+                <div className={styles.d_preview} style={{ "width": dLength }} />
+                <div className={styles.f_preview} style={{ "width": fLength }} />
+
+
+            </div>
+            <div className={styles.published_tile_text}>{name}
+                <div className={styles.published_tile_description}>{description}</div>
+            </div>
 
         </div >
     )
