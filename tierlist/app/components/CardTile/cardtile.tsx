be4537724be807cfccaac9c7a80ba0ef5267423b
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-05-14 15:48:39 -0700

    Added card tiles to create page

--- tierlist/app/components/CardTile/cardtile.tsx
+++ tierlist/app/components/CardTile/cardtile.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-// import styles from "./PublishedTile.module.css";
+import styles from "./CardTile.module.css";
 import { useEffect, useState } from "react";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,14 +7,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface CardTileProps {
     name: string,
-    description: string,
     image_url: string
 }
 
-export default function PublishedTile({ name, description, image_url }: CardTileProps) {
+export default function CardTile({ name, image_url }: CardTileProps) {
 
     return (
-        <div>
+        <div className={styles.cardtile}>
             {name}
         </div>
     )
--- tierlist/app/main/published/create/[template]/page.tsx
+++ tierlist/app/main/published/create/[template]/page.tsx
@@ -1,21 +1,32 @@
 'use client';
 // import styles from './Select.module.css'
-import TemplateTile from '@/app/components/TemplateTile/templatetile'
 import { useAppSelector, useAppDispatch } from '@/app/redux/store'
 import { useEffect, useState } from 'react'
 import { thunkGetAllTemplates } from '@/app/redux/alllists'
 import { useParams } from 'next/navigation';
+import CardTile from '@/app/components/CardTile/cardtile';
+
+interface Template {
+    id: number,
+    background_image_url: string,
+    description: string,
+    name: string,
+    owner: number,
+    public: boolean,
+    cards: []
+}
 
 export default function Create() {
 
     const templates = useAppSelector(state => state.allLists.templates)
-    const [template, setTemplate] = useState({})
+    const [template, setTemplate] = useState<Template>()
     const [disabled, setDisabled] = useState(true)
     const params = useParams()
 
+
     useEffect(() => {
         const templateId = Number(params.template)
-        if (templates[templateId]) {
+        if (templates) {
             setTemplate(templates[templateId])
         }
         console.log(template)
@@ -37,6 +48,14 @@ export default function Create() {
     return (
         <div >
             Create page
+            {template?.cards && Object.values(template.cards).map((card: any) => (
+
+                <CardTile
+                    key={`card ${card[0]}`}
+                    name={card[1]}
+                    image_url={card[2]}
+                />
+            ))}
         </div>
     )
 }
