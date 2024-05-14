ac4beee72664a51bc95f11c57bce24ff2b581fb9
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-05-14 16:50:01 -0700

    Display card tile images

--- tierlist/app/components/CardTile/cardtile.tsx
+++ tierlist/app/components/CardTile/cardtile.tsx
@@ -3,6 +3,7 @@ import styles from "./CardTile.module.css";
 import { useEffect, useState } from "react";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 
 
 interface CardTileProps {
@@ -15,6 +16,12 @@ export default function CardTile({ name, image_url }: CardTileProps) {
     return (
         <div className={styles.cardtile}>
             {name}
+            <Image
+                src={image_url}
+                alt={name}
+                width={40}
+                height={40}
+            />
         </div>
     )
 }
