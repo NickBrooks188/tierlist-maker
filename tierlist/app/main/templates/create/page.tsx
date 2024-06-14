676e000c4c5e7240a80731656140356fba523441
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-06-14 11:31:21 -0700

    Template creation page complete

--- tierlist/app/main/templates/create/page.tsx
+++ tierlist/app/main/templates/create/page.tsx
@@ -9,6 +9,8 @@ import { useAppDispatch } from '@/app/redux/store';
 import CardTile from '@/app/components/CardTile/cardtile';
 import Link from 'next/link';
 import { faChevronLeft, faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from 'next/navigation';
+
 
 export default function Page() {
     const [name, setName] = useState<string>('')
@@ -16,7 +18,9 @@ export default function Page() {
     const [image, setImage] = useState<File | null>(null)
     const [openModal, setOpenModal] = useState<boolean>(false)
     const [cards, setCards] = useState<any[]>([])
-    const [disabled, setDisabled] = useState<boolean>(true)
+    const [nextDisabled, setNextDisabled] = useState<boolean>(true)
+    const [publishDisabled, setPublishDisabled] = useState<boolean>(true)
+    const router = useRouter()
 
     const dispatch = useAppDispatch()
 
@@ -26,11 +30,20 @@ export default function Page() {
 
     useEffect(() => {
         if (name && description && image) {
-            setDisabled(false)
+            setNextDisabled(false)
         } else {
-            setDisabled(true)
+            setNextDisabled(true)
         }
     }, [name, description, image])
+
+
+    useEffect(() => {
+        if (cards.length > 0) {
+            setPublishDisabled(false)
+        } else {
+            setPublishDisabled(true)
+        }
+    }, [cards])
 
     const handleSubmit = async () => {
 
@@ -50,6 +63,11 @@ export default function Page() {
         }
         const serverData = await dispatch(thunkCreateTemplate(template))
         console.log(serverData)
+        if (serverData.errors) {
+            console.log(serverData.errors)
+        } else {
+            router.push(`/main`)
+        }
     }
 
     const scrollRight = (e: any) => {
@@ -84,22 +102,25 @@ export default function Page() {
                         accept="image/*"
                         onChange={(e) => setImage(e.target.files ? e.target.files[0] : null)}
                     />
-                    <button className='button-light' disabled={disabled} onClick={scrollRight}>Next<FontAwesomeIcon icon={faAngleRight} /></button>
+                    <button className='button-light' disabled={nextDisabled} onClick={scrollRight}>Next<FontAwesomeIcon icon={faAngleRight} /></button>
                 </form>
                 <div className={styles.section_wrapper}>
-                    <button className='button-dark' onClick={scrollLeft}><FontAwesomeIcon icon={faAngleLeft} />Back to details</button>
-                    <h3>Tier list items</h3>
+                    <button className='button-light' onClick={scrollLeft}><FontAwesomeIcon icon={faAngleLeft} />Back to details</button>
+                    <h3 className={styles.cards_header}>Tier list items</h3>
                     <div className={styles.divider} />
-                    {(cards.length > 0) && cards.map((card: any, cardIndex: number) => (
+                    <div className={styles.cards_wrapper}>
+                        {(cards.length > 0) && cards.map((card: any, cardIndex: number) => (
 
-                        <CardTile
-                            key={cardIndex}
-                            name={card.name || ''}
-                            image_url={card.image_url || ''}
-                        />
-                    ))}
+                            <CardTile
+                                key={cardIndex}
+                                name={card.name || ''}
+                                image_url={card.image_url || ''}
+                            />
+                        ))}
+                        {cards.length === 0 && <div>Please add tier list items</div>}
+                    </div>
                     <button className='button-light' onClick={() => setOpenModal(true)}><FontAwesomeIcon icon={faPlus} />Add tier list item</button>
-                    <button className='button-dark' disabled={disabled} onClick={handleSubmit}>Publish tier list template</button>
+                    <button className='button-dark' disabled={publishDisabled || nextDisabled} onClick={handleSubmit}>Publish tier list template</button>
                 </div>
                 {(openModal) &&
                     (
