5089d82142b1dbb0ec1740c64b705d40caa2d32c
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-06-14 11:14:12 -0700

    Slideshow format for template creation

--- tierlist/app/main/templates/create/page.tsx
+++ tierlist/app/main/templates/create/page.tsx
@@ -8,7 +8,7 @@ import { thunkCreateTemplate, uploadImage } from "@/app/redux/onelist";
 import { useAppDispatch } from '@/app/redux/store';
 import CardTile from '@/app/components/CardTile/cardtile';
 import Link from 'next/link';
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft, faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Page() {
     const [name, setName] = useState<string>('')
@@ -40,7 +40,6 @@ export default function Page() {
             console.log(imageData.errors)
             return
         }
-        console.log('~~~~~~~~', imageData)
 
         const template = {
             name,
@@ -53,13 +52,28 @@ export default function Page() {
         console.log(serverData)
     }
 
+    const scrollRight = (e: any) => {
+        e.preventDefault()
+        const mainWrapper = document.getElementById('main-wrapper')
+        if (mainWrapper) {
+            mainWrapper.scrollBy({ left: 500, top: 0, behavior: 'smooth' })
+        }
+    }
+
+    const scrollLeft = () => {
+        const mainWrapper = document.getElementById('main-wrapper')
+        if (mainWrapper) {
+            mainWrapper.scrollBy({ left: -500, top: 0, behavior: 'smooth' })
+        }
+    }
+
     return (
         <>
             <Link href='/main'>
                 <div className="back_main"><FontAwesomeIcon icon={faChevronLeft} />Back</div>
             </Link>
-            <div className={styles.main_wrapper}>
-                <form>
+            <div className={styles.main_wrapper} id='main-wrapper'>
+                <form className={styles.section_wrapper}>
                     <label>Template name<span className='asterisk'>*</span></label>
                     <input type='text' name='name' value={name} onChange={e => setName(e.target.value)}></input>
                     <label>Template description<span className='asterisk'>*</span></label>
@@ -70,8 +84,12 @@ export default function Page() {
                         accept="image/*"
                         onChange={(e) => setImage(e.target.files ? e.target.files[0] : null)}
                     />
+                    <button className='button-light' disabled={disabled} onClick={scrollRight}>Next<FontAwesomeIcon icon={faAngleRight} /></button>
                 </form>
-                <div className={styles.cards_wrapper}>
+                <div className={styles.section_wrapper}>
+                    <button className='button-dark' onClick={scrollLeft}><FontAwesomeIcon icon={faAngleLeft} />Back to details</button>
+                    <h3>Tier list items</h3>
+                    <div className={styles.divider} />
                     {(cards.length > 0) && cards.map((card: any, cardIndex: number) => (
 
                         <CardTile
@@ -80,8 +98,8 @@ export default function Page() {
                             image_url={card.image_url || ''}
                         />
                     ))}
-
                     <button className='button-light' onClick={() => setOpenModal(true)}><FontAwesomeIcon icon={faPlus} />Add tier list item</button>
+                    <button className='button-dark' disabled={disabled} onClick={handleSubmit}>Publish tier list template</button>
                 </div>
                 {(openModal) &&
                     (
@@ -89,7 +107,6 @@ export default function Page() {
                             <CreateCardModal addCard={addCard} setOpenModal={setOpenModal} />
                         </div>
                     )}
-                <button className='button-dark' disabled={disabled} onClick={handleSubmit}>Create tier list template</button>
             </div>
 
         </>
