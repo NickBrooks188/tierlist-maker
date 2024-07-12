2970f372c21ea5b51b8afa8619f7e7b4f952468b
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-07-12 14:50:05 -0700

    Build

--- tierlist/app/main/layout.tsx
+++ tierlist/app/main/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         } else {
             dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
         }
-    }, [dispatch]);
+    }, [dispatch, token]);
 
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
