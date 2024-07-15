68e98a9e163757909e571234b676d2e80c15f094
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-07-15 16:15:27 -0700

    Update next.config.mjs

--- tierlist/next.config.mjs
+++ tierlist/next.config.mjs
@@ -5,7 +5,10 @@ const nextConfig = {
         domains: ['jello-bucket.s3.us-west-1.amazonaws.com', 'tierforge.s3.us-west-1.amazonaws.com']
     },
     reactStrictMode: false,
-    output: "standalone"
+    output: "standalone",
+    future: {
+        webpack5: false,
+    },
 };
 
 export default nextConfig;
