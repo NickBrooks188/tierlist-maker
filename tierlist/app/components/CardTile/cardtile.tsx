8edbd79770b8603d42bd495f2fd4e4b449a1544d
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-07-15 16:25:03 -0700

    Update cardtile.tsx

--- tierlist/app/components/CardTile/cardtile.tsx
+++ tierlist/app/components/CardTile/cardtile.tsx
@@ -1,5 +1,4 @@
-// import styles from "./cardTile.module.css";
-// import styles from "./cardTile.module.css";
+import styles from "./cardtile.module.css";
 
 
 interface CardTileProps {
@@ -11,8 +10,8 @@ export default function CardTile({ name, image_url }: CardTileProps) {
 
     return (
 
-        <div >
-            <div
+        <div className={styles.card_tile}>
+            <div className={styles.card_image}
                 style={{
                     "backgroundImage": `url(${image_url})`,
                     'backgroundSize': `auto 100%`,
@@ -21,7 +20,7 @@ export default function CardTile({ name, image_url }: CardTileProps) {
                 }}
             >
             </div>
-            <div >{name}</div>
+            <div className={styles.card_name}>{name}</div>
         </div>
     )
 }
