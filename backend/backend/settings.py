e133aa7b915a6d3b70315227b1339ec6f20f2f37
Nick Brooks <nicholas.brooks@aya.yale.edu>
2024-08-21 17:32:20 -0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://tierforge.onrender.com',
-    'https://tierforge.vercel.app/'
+    'https://tierforge.vercel.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
